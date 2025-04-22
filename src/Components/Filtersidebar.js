7297117042d6a0363e39aa27b5d7534661671668
Megha gotawala <programmer95.dynamicdreamz@gmail.com>
2025-04-22 13:03:10 +0530

    issue solve

--- src/Components/Filtersidebar.js
+++ src/Components/Filtersidebar.js
@@ -3,24 +3,30 @@ import React from 'react';
 const FilterSection = ({ title, items, selectedFilters, filterKey, handleCheckboxChange }) => {
   return (
     <div className="widget blockid_brand">
-      <h5 className="widget-title">By {title}</h5>
-      <div className="loke_scroll">
-        <ul className="nt_filter_block nt_filter_color css_ntbar">
-          {items.map((item, i) => (
-            <li key={i}>
-              <label className='sidebar-filter'>
-                <input
-                  type="checkbox"
-                  checked={selectedFilters[filterKey]?.includes(item)}
-                  onChange={() => handleCheckboxChange(filterKey, item)}
-                />
-                {item}
-              </label>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+  <h5 className="widget-title">By {title}</h5>
+  <div className="loke_scroll">
+    <ul className="nt_filter_block nt_filter_color css_ntbar">
+      {items.map((item, i) => {
+        const inputId = `${filterKey}-${item}`;
+        return (
+          <li key={i}>
+            <label className='sidebar-filter' htmlFor={inputId}>
+              <input
+                id={inputId}
+                name={filterKey}
+                type="checkbox"
+                checked={selectedFilters[filterKey]?.includes(item)}
+                onChange={() => handleCheckboxChange(filterKey, item)}
+              />
+              {item}
+            </label>
+          </li>
+        );
+      })}
+    </ul>
+  </div>
+</div>
+
   );
 };
 
