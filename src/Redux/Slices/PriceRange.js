3977365c986d7e37738ac1cc9371551e4ac0e447
Megha gotawala <programmer95.dynamicdreamz@gmail.com>
2025-07-25 18:37:22 +0530

    pricerange

--- src/Redux/Slices/PriceRange.js
+++ src/Redux/Slices/PriceRange.js
@@ -4,10 +4,10 @@ const initialState = {
   min: null,
   max: null,
   displayMin: null,
-  displayMax: null
+  displayMax: null,
 };
 
-export const priceRangeSlice = createSlice({
+const priceRangeSlice = createSlice({
   name: 'priceRange',
   initialState,
   reducers: {
@@ -24,20 +24,20 @@ export const priceRangeSlice = createSlice({
       state.max = null;
       state.displayMin = null;
       state.displayMax = null;
-    }
-  }
+    },
+  },
 });
 
 export const { setRange, setDisplayRange, resetRange } = priceRangeSlice.actions;
 
 export const selectPriceRange = (state) => ({
   min: state.priceRange.min,
-  max: state.priceRange.max
+  max: state.priceRange.max,
 });
 
 export const selectDisplayRange = (state) => ({
   min: state.priceRange.displayMin,
-  max: state.priceRange.displayMax
+  max: state.priceRange.displayMax,
 });
 
 export default priceRangeSlice.reducer;
